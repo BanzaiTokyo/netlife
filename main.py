706b8569d19b023f364052b5f731971b67ae2161
banzaitokyo <banzaitokyo@gmail.com>
2013-12-17 18:01:50 +0100

    udalil starye fajly

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ import random, ast
 
 gameColumns = 20
 gameRows = 15
+
 buttonlist = {}     #key => button id, value => button instance
 fieldState = {}     #key => cell coordinates(tuple), value => Cell class instance
 clickedcells = {}   #key => button id, value => number of clicks
 
-counter = 0
+'''status
+    1 = neighbour
+    9 = dead
+    10+ = live
+'''
+
+livecellsnum = 0
 count = 0
 
 class Cell(Button):
@@ -30,100 +37,106 @@ class Cell(Button):
     def status(self, status):
          self._status = status
 
-    '''def getstatus(self):
-        return self.status
 
-     def setstatus(self, status):
-        self.status = status
-        return self.status'''
+    def updatestatus(self, clicks):
 
-    def updatestatus(self, coordinates, clicks):
+        if self.status == 1 and clicks >= 3:
+            self.status = 14
+        if self.status >= 10 and clicks > 0:
+            self.status = 14
+        if self.status >= 10 and clicks == 0:
+            self.status -= 1
 
-        if buttonlist[coordinates].status == "neighbour" and clicks >= 3:
-            buttonlist[coordinates].status = "alive"
-        return self.status
 
-    def drawstatus(self, coordinates):
-        if "live" in buttonlist[coordinates].status:
-            buttonlist[coordinates].background_color = [0.5, 2, 0.5, 1]
-        if "neighbour" in buttonlist[coordinates].status:
-            buttonlist[coordinates].background_color = [0.5, 0.5, 0.5, 1]
-        if "dead" in buttonlist[coordinates].status:
-            buttonlist[coordinates].background_color = [1, 1, 1, 1]
 
-    def updatefield(self, coordinates):
+
+
+    def drawstatus(self):
+        if self.status == 14:
+            self.background_color = [0.5, 2, 0.5, 1]
+        if self.status == 13:
+            self.background_color = [0.5, 1.8, 0.5, 1]
+        if self.status == 12:
+            self.background_color = [0.5, 1.5, 0.5, 1]
+        if self.status == 11:
+            self.background_color = [0.5, 1.2, 0.5, 1]
+        if self.status == 10:
+            self.background_color = [0.5, 0.9, 0.5, 1]
+
+        if self.status == 1:
+            self.background_color = [0.5, 0.5, 0.5, 1]
+        if self.status == 9:
+            self.background_color = [1, 1, 1, 1]
+
+    def updatefield(self, id):
 
         count = 0
-        t = ast.literal_eval(coordinates)
+        t = ast.literal_eval(id)
         c3 = [-1, 0, 1]
         for x in c3:
             for y in c3:
                 if x == 0 and y == 0: #exclude point itself
                     continue
                 try:
-                    if "alive" in buttonlist[str([t[0]+x,t[1]+y])].status: count +=1
+                    if buttonlist[str([t[0]+x,t[1]+y])].status >= 10: count +=1
                 except:
                     pass
 
 
-        if count >= 1 and buttonlist[coordinates].status == "dead":
-            buttonlist[coordinates].status = "neighbour"
-        if count >= 1 and buttonlist[coordinates].status == "neighbour":
-            buttonlist[coordinates].status = "neighbour"
-        if count == 0 and buttonlist[coordinates].status == "neighbour":
-            buttonlist[coordinates].status = "dead"
-        if count == 0 and buttonlist[coordinates].status == "dead":
-            buttonlist[coordinates].status = "dead"
+        if count >= 1 and self.status == 9:
+            self.status = 1
+        if count == 0 and self.status == 1:
+            self.status = 9
 
-        return self.status
+
+
+
 
 
 def calculateMove(clickedcells):
     '''
 
     '''
-    print "next step"
+    global livecellsnum
+    livecellsnum = 0
+    #print "next step"
     for x in clickedcells.iterkeys():
-
-        buttonlist[x].updatestatus(x, clickedcells[x])
+        buttonlist[x].updatestatus(clickedcells[x])
 
     for x in buttonlist:
         buttonlist[x].updatefield(x)
-        buttonlist[x].drawstatus(x)
-
+        buttonlist[x].drawstatus()
+        if buttonlist[x].status >= 10: livecellsnum += 1
+    print "number of live cells:", livecellsnum
 
 
 def callback(instance):
     clickedcells[instance.id] = clickedcells.get(instance.id, 0) + 1
 
 
-    print('The button %s is being pressed' % instance.id)
+    #print('The button %s is being pressed' % instance.id)
 
 
 
 class LifeApp(App):
     counter = 0
-    buttonID = Button(id = "_" + str(random.randrange(gameColumns*gameRows)))
     def build(self):
         layout = GridLayout(cols=gameColumns, rows = gameRows)
         for i in range(gameRows*gameColumns):
-
             x = [i%gameColumns, i/gameColumns]
-
-            btn = Cell(text='', id=str(x), status="neighbour")
-
+            btn = Cell(text='', id=str(x), status=1)
             buttonlist[btn.id]=btn
             layout.add_widget(btn)
             btn.bind(on_press=callback)
-            #fieldState[str(x)] = Cell("neighbour")
+
         #make a live cell
-        buttonlist[str([random.randrange(gameColumns), random.randrange(gameRows)])].status = "alive"
+        buttonlist[str([random.randrange(gameColumns), random.randrange(gameRows)])].status = 14
 
         Clock.schedule_interval(self.randomly, 3)
         return layout
 
     def randomly(self, dt):
-        buttonlist[str([random.randrange(gameColumns), random.randrange(gameRows)])].status = "alive"
+        #buttonlist[str([random.randrange(gameColumns), random.randrange(gameRows)])].status = 14
         calculateMove(clickedcells)
         for i in buttonlist.iterkeys():
             clickedcells[i] = 0
